45aa63053894f848f5835897e7a83f63976f7f11
Alvin strandberg <Str4ndb3rg@live.se>
2023-08-25 11:16:10 +0200

    Deleted MVC from project folder.

--- Program.cs
+++ Program.cs
@@ -25,6 +25,7 @@ namespace BookLibraryApi
 
             builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
             builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
             builder.Services.AddScoped<GenreRepository>();
             builder.Services.AddScoped<FilterRepository>();
